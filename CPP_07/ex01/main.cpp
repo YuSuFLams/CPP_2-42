0f12ffe9f5ee3d2d422f839a700e86b0c9e694f1
ylamsiah <110131779+YuSuFLams@users.noreply.github.com>
2024-01-13 19:59:36 +0100

    push in cpp07

--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -12,41 +12,76 @@
 
 #include "iter.hpp"
 
-void upper_ca(char &c)
+// void upper_ca(char &c)
+// {
+//     if (c >= 'a' && c <= 'z')
+//         c -= 32;
+//     else if (c >= 'A' && c <= 'Z')
+//         c += 32;
+// }
+
+// template <typename T>
+// void test_case(T &pos)
+// {
+//     std::cout << pos << ((pos < 0) ? ": negative" : " : positive") << std::endl;
+// }
+
+// int main()
+// {
+//     std::cout << "**** First Test ****" << std::endl;
+//     char str[] = "hello";
+
+//     std::cout << str << std::endl;
+//     ::iter(str, strlen(str), upper_ca);
+//     std::cout << str << std::endl;
+
+//     std::cout << "**** Second Test ****" << std::endl;
+    
+//     char *new_str = NULL;
+    
+//     ::iter(new_str, 3, upper_ca);
+    
+//     std::cout << "**** Third Test ****" << std::endl;
+    
+//     int array[] = {12, 23, 4, 34, -20};
+//     int len = sizeof(array) / sizeof(*array); 
+
+//     ::iter(array, len, &test_case<int>);
+    
+//     return 0;
+// }
+
+
+class Awesome
 {
-    if (c >= 'a' && c <= 'z')
-        c -= 32;
-    else if (c >= 'A' && c <= 'Z')
-        c += 32;
+	public:
+		Awesome( void ) : _n( 42 ) { return; }
+		int get( void ) const { return this->_n; }
+	private:
+		int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+	o << rhs.get();
+	return o;
 }
 
-template <typename T>
-void test_case(T &pos)
+template< typename T >
+void print( T& x )
 {
-    std::cout << pos << ((pos < 0) ? ": negative" : " : positive") << std::endl;
+	std::cout << x << std::endl;
+	return ;
 }
 
 int main()
 {
-    std::cout << "**** First Test ****" << std::endl;
-    char str[] = "hello";
+	int tab[] = { 0, 1, 2, 3, 4 };
 
-    std::cout << str << std::endl;
-    ::iter(str, strlen(str), upper_ca);
-    std::cout << str << std::endl;
+	Awesome tab2[5];
 
-    std::cout << "**** Second Test ****" << std::endl;
-    
-    char *new_str = NULL;
-    
-    ::iter(new_str, 3, upper_ca);
-    
-    std::cout << "**** Third Test ****" << std::endl;
-    
-    int array[] = {12, 23, 4, 34, -20};
-    int len = sizeof(array) / sizeof(*array); 
+	iter( tab, 5, print<const int> );
+	iter( tab2, 5, print<Awesome> );
 
-    ::iter(array, len, &test_case<int>);
-    
-    return 0;
+	return 0;
 }
--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -12,47 +12,98 @@
 
 #include "Array.hpp"
 
-int main()
-{
-    {
-        try
-        {
-            Array<int> obj(9);
+// int main()
+// {
+//     {
+//         try
+//         {
+//             Array<int> obj(9);
             
-            int obj1[9] = {15, -9, -8, 5, 0, 7, -99, 26, 11};
+//             int obj1[9] = {15, -9, -8, 5, 0, 7, -99, 26, 11};
             
-            for(unsigned int i = 0; i < obj.size(); i++)
-                obj[i] = obj1[i];
-            std::cout << "Range Of Array is : " << obj.size() << std::endl;
-            for(unsigned int i = 0; i < obj.size(); i++)
-                std::cout << obj[i] << "\n";
-            obj[11];
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << std::endl;
-        }
-    }
-    std::cout << std::endl;
-    {
-        try
-        {
-            Array<std::string> obj(7);
+//             for(unsigned int i = 0; i < obj.size(); i++)
+//                 obj[i] = obj1[i];
+//             std::cout << "Range Of Array is : " << obj.size() << std::endl;
+//             for(unsigned int i = 0; i < obj.size(); i++)
+//                 std::cout << obj[i] << "\n";
+//             obj[11];
+//         }
+//         catch(const std::exception& e)
+//         {
+//             std::cerr << e.what() << std::endl;
+//         }
+//     }
+//     std::cout << std::endl;
+//     {
+//         try
+//         {
+//             Array<std::string> obj(7);
             
-            std::string obj1[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+//             std::string obj1[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
             
-            for(unsigned int i = 0; i < obj.size(); i++)
-                obj[i] = obj1[i];
-            std::cout << "Range Of Array is : " << obj.size() << std::endl;
-            for(unsigned int i = 0; i < obj.size(); i++)
-                std::cout << obj[i] << "\n";
-            std::cout << obj[-21] << std::endl;
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << std::endl;
-        }
+//             for(unsigned int i = 0; i < obj.size(); i++)
+//                 obj[i] = obj1[i];
+//             std::cout << "Range Of Array is : " << obj.size() << std::endl;
+//             for(unsigned int i = 0; i < obj.size(); i++)
+//                 std::cout << obj[i] << "\n";
+//             std::cout << obj[-21] << std::endl;
+//         }
+//         catch(const std::exception& e)
+//         {
+//             std::cerr << e.what() << std::endl;
+//         }
         
+//     }
+//     return 0;
+// }
+#define MAX_VAL 750
+
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
     }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return 0;
 }
